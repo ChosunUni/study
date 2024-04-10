2840b76783f6b8e9d1c1cefbf69ee52d2eec16ec
Wandongkinn <162245767+Wandongkinn@users.noreply.github.com>
2024-04-10 19:22:02 +0900

    Delete study directory

--- study/src/main/java/com/suntime/study/repository/TimerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.suntime.study.repository;
-
-import com.suntime.study.entity.TimerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimerRepository extends JpaRepository<TimerEntity, Long> {
-}
